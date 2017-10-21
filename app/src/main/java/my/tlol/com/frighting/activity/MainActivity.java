4bc86c35972bce6f0325de24fdc8d4b5e03d4444
xiaoxin2014 <184689929@qq.com>
2017-10-21 20:04:14 +0800

    111

--- app/src/main/java/my/tlol/com/frighting/activity/MainActivity.java
+++ app/src/main/java/my/tlol/com/frighting/activity/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TabHost;
-//
 import android.widget.TextView;
 import android.widget.Toast;
 
